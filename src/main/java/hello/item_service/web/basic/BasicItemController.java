ce71136047924b9d125ccb2f269c184038f091cc
limjungmin89 <ckzkboy@naver.com>
2025-04-08 10:53:01 +0900

    1. 아이템 등록 추가 2. 아이템 수정 추가

--- src/main/java/hello/item_service/web/basic/BasicItemController.java
+++ src/main/java/hello/item_service/web/basic/BasicItemController.java
@@ -4,11 +4,10 @@ import hello.item_service.domain.Item;
 import hello.item_service.domain.ItemRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,6 +22,7 @@ import java.util.List;
  * -----------------------------------------------------------
  * 2025. 4. 7.        mzc01-jungminim       최초 생성
  */
+@Slf4j
 @Controller
 @RequestMapping("/basic/items")
 @RequiredArgsConstructor
@@ -44,6 +44,77 @@ public class BasicItemController {
         model.addAttribute("item", item);
 
         return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm() {
+        return "/basic/addForm";
+    }
+
+//    @PostMapping("/add")
+    public String saveRequestParam(
+            @RequestParam("itemName") String itemName,
+            @RequestParam("price") int price,
+            @RequestParam("quantity") int quantity,
+            Model model
+    ) {
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute(item);
+
+        return "/basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String saveModelAttirbuteV1(@ModelAttribute("item") Item item) {
+        log.info("ModelAttribute >>> {}" , item);
+
+        itemRepository.save(item);
+
+        return "/basic/item";
+    }
+
+    @PostMapping("/add")
+    public String saveModelAttirbuteV2(@ModelAttribute Item item) {
+        log.info("ModelAttribute >>> {}" , item);
+
+        itemRepository.save(item);
+
+        return "/basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String saveModelAttirbuteV3(Item item) {
+        log.info("ModelAttribute >>> {}" , item);
+
+        itemRepository.save(item);
+
+        return "/basic/item";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        log.info("ModelAttribute >>> {}" , model);
+
+        Item item = itemRepository.findById(itemId);
+
+        model.addAttribute("item", item);
+
+        return "/basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        log.info("ModelAttribute >>> {}" , item);
+
+        itemRepository.update(itemId, item);
+
+        return "redirect:/basic/items/{itemId}";
     }
 
     @PostConstruct
